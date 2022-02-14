7edd9d315fc6611d55cd5ffd6665979200f4417f
Samuel Levi Araújo Alves <samuel.levi@senior.com.br>
2022-02-14 12:10:02 -0300

    CapChallenge - Escadas

---
+++ src/Questoes/Stairs.java
@@ -0,0 +1,24 @@
+package Questoes;
+
+import java.util.Scanner;
+
+public class Stairs {
+
+    public static void escada() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a altura da escada: ");
+        int alturaDaEscada = sc.nextInt();
+
+        for (int i = 1; i <= alturaDaEscada; i++) {
+            for (int k = 1; k <= alturaDaEscada - i + 1; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ src/application/Challenge.java
@@ -0,0 +1,11 @@
+package application;
+
+import Questoes.Stairs;
+
+public class Challenge {
+    public static void main(String[] args) {
+        Stairs.escada();
+    }
+
+
+}
